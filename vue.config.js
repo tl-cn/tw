cc533d06a9315136ee50d884e530a557f999529c
ZQH <>
2021-04-29 12:33:46 +0800

    new change

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    publicPath: './',
-    chainWebpack: config => {
-        config.plugin('html').tap(args => {
-            args[0].title = '打字练习';
-            return args;
-        })
-    }
-}
